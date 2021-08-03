6130505256659bc4a2b9bed42efd0feebe232692
Gabriel <gabriel.gabrielyea@gmail.com>
2021-08-02 22:33:50 -0500

    Add access methods

--- src/scripts/api/api-access.js
+++ src/scripts/api/api-access.js
@@ -1,4 +1,27 @@
 class ApiAccess {
+  request = (url, params = {}, method = 'GET') => {
+    let options = {
+      method,
+    };
+    if ('GET' === method) {
+      url += '?' + (new URLSearchParams(params)).toString();
+    } else {
+      options.body = JSON.stringify(params);
+    }
+
+    return fetch(url, options);
+  };
+
+  get = async (url, params) => {
+    const response = await this.request(url, params, 'GET');
+    return response.json();
+  };
+
+  post = async (url, params) => {
+    const response = await this.request(url, params, 'POST');
+    return response.json();
+  };
+
   requestApi = async (url, id) => {
     const response = await fetch(`${url}/${id}`);
     return response.json();
