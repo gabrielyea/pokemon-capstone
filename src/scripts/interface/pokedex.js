7051ff5e0f0cab75db892797bda9fb06ff639bdd
d4nielj <d4niel.djm@gmail.com>
2021-08-04 11:24:56 -0500

    Add pkm descriptions and types to modal

--- src/scripts/interface/pokedex.js
+++ src/scripts/interface/pokedex.js
@@ -1,5 +1,24 @@
+import routes from '../api/api-routes.js';
+import access from '../api/api-access.js';
+
 class Pokedex {
-  
+  getData = async (name) => {
+    const response = await access.getApi(`${routes.POKEDEX}${name}`, {});
+    return response;
+  }
+
+  transformDesc = (response) => {
+    let pkmDesc = response.flavor_text_entries[1].flavor_text;
+    pkmDesc = pkmDesc.replace('\n', ' ');
+    pkmDesc = pkmDesc.replace('\f', ' ');
+    return pkmDesc;
+  }
+
+  getDesc = async (name) => {
+    const response = await this.getData(name);
+    const pkmDesc = this.transformDesc(response);
+    return pkmDesc;
+  }
 }
 
 const pokedex = new Pokedex();
